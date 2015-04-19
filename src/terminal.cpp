2b1e96007e835ca5afaba613e182c4aaec49b3e2
Peter Hung <phung001@hammer.cs.ucr.edu>
2015-04-18 18:14:01 -0700

    adjusted parsing and execvp and worked on Makefile

--- src/terminal.cpp
+++ src/terminal.cpp
@@ -3,40 +3,76 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <iostream>
-#include <boost/tokenizer.hpp> 
+#include <string.h> 
+#include <sys/wait.h>
+#include <vector>
 
 using namespace std;
-using namespace boost;
 
-int main(int argc, char* argv[]){
-	
-	while(1){
-		char host[64];
+void outhostname(){
+		char host[32];
+		int error;
 
 	//output login info
-		if(gethostname(host, 64) == -1) perror("gethostname");
-		else{
+		error = gethostname(host, 32);
+		if(error == -1)
+			perror("gethostname");
+		else{ 
 			cout << getlogin() << '@' << host << "$ ";
 		}
+}
 
-	//get input
+int main(int argc, char* argv[]){
+	while(1){
+		outhostname();
+		//get input
 		string userinput;
-		cin >> userinput;
-	
-	//tokenize here
-		char_separator<char> delim(",.<>?/:[]{}()-\\_\"=+!@#$%^*`~\t\n ","|&;"); 	
-		tokenizer<char_separator<char>> mytok(userinput, delim);
+		getline(cin, userinput);
+		char *token;
 
-		for(auto i = mytok.begin(); i != mytok.end(); ++i){
+		//tokenize here	
+		vector<char*> tokenlist;				
 	
-			if(*i  == "exit") 
-				exit(0); 	
-	
-			//else if(execvp(userinput, 0) == -1) perror(execvp);
-
-			else perror("not a valid command");
+//find ; && || and add space if needed
 		
+		char *uinput = new char[userinput.length() +1];	//turns string into c*
+		strcpy(uinput, userinput.c_str());
+
+		token = strtok(uinput, " ");				//tokenize and add to vector of char*
+		while(token != NULL){
+			tokenlist.push_back(token);
+			token = strtok(NULL, " ");	
 		}
+
+		tokenlist.push_back('\0');
+
+		char **argg = &tokenlist[0]; 
+
+//		for(unsigned int j = 0; j < tokenlist.size(); ++j)	
+//			cout << j << tokenlist.at(j) << endl;	
+
+		for(unsigned int j = 0; j < tokenlist.size(); ++j){	//execute loop
+			
+			if(strcmp(tokenlist.at(j), "exit") == 0)
+				exit(0); 	
+				
+			else if(execvp(argg[0], argg) == -1){
+				perror("execvp");
+			}
+		//	else if(tokenlist.at(j) == "true");
+
+			//else if(tokenlist.at(j) == "false") exit(0);
+
+		//	else if(tokenlist.at(j) == "&" && tokenlist.at(j+1) == "&");
+				
+		//	else if(tokenlist.at(j) == "|" && tokenlist.at(j+1) == "|");
+
+			else {
+				perror("not a valid command");
+			}
+		}
+		delete [] uinput;
+		
 	}
 	return 0;
 }
