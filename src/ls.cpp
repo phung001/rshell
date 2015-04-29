413bad3b86bd47aa6996ec14a47339b0827db392
Peter Hung <phung001@hammer.cs.ucr.edu>
2015-04-28 23:38:47 -0700

    ls and ls-a

--- src/ls.cpp
+++ src/ls.cpp
@@ -1,16 +1,20 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include <iostream>
 #include <string.h> 
 #include <sys/wait.h>
+#include <sys/stat.h>
 #include <sys/types.h>
+#include <dirent.h>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
-tring trim_space(string const& str){
+string trim_space(string const& str){
 		if(str.empty()) return str;
 
 			size_t first = str.find_first_not_of(' ');
@@ -32,35 +36,73 @@ string trim(string const& str){
 				return temp;
 }
 
-void outhostname(){
-	char host[32];
-	int error;
-
-	error = gethostname(host, 32);
-	if(error == -1)	perror("gethostname");
-	else cout << getlogin() << '@' << host << "$ ";
+bool sort_alpha(char* a, char* b){
+	return (tolower(static_cast<unsigned char>(a[0])) < tolower(static_cast<unsigned char>(b[0])));
 }
 
-int main(){
-	while(){
-		outhostname();
-		string userinput;
-		getline(cin, userinput);
+void ls_go(char *argv, bool a, bool l, bool R){
+	DIR *dirp;
 
-		while(!userinput.empty()){
+	if(NULL == (dirp = opendir(argv))){
+		perror("Error with opendir");
+		exit(1);
+	}
 	
-			if(first_char > userinput.size()) break; 
-				userinput = trim(userinput);
-				
+	struct dirent *filespecs;
+	vector<char*> list;	
+	errno = 0;
+	while(NULL != (filespecs = readdir(dirp))){
+		if(!a){
+			if(filespecs->d_name[0] != '.'){
+				list.push_back(filespecs->d_name);
+			}
+		}
+		else
+			list.push_back(filespecs->d_name);
+	}
+	sort(list.begin(), list.end(), sort_alpha);
+	for(unsigned int i = 0; i < list.size(); i++){
+		cout <<list.at(i) << "  ";
+	}
+	cout << endl;
+
+	if(errno != 0){
+		perror("Error with readdir");
+		exit(1);
+	}
+
+	if(closedir(dirp) == -1){
+		perror("Error with closedir");
+		exit(1);
+	}
+}
+
+
+
+
+int main(int argc, char** argv){
+
+	char here[1] = {'.'};
+	if(argc == 1){
+		// do ls here
+		ls_go(here, false, false ,false);		
+	}
+	else{
+		if(strcmp(argv[1], "-a") == 0) ls_go(here, true, false, false);
+
+		
+		
+		else{
+			cout << "nothing" << endl;
+
+
 
 		}
-	
+	}
 
-	}	
 
 	return 0;
 }
 		
 		
-		
-		
+	
