cd86dbb1f4efebc21c6463b70e527a50dbd4f866
Peter Hung <phung001@hammer.cs.ucr.edu>
2015-04-30 10:42:13 -0700

    completed -R, works as -Ra

--- src/ls.cpp
+++ src/ls.cpp
@@ -18,7 +18,7 @@ bool sort_alpha(char* a, char* b){
 	return (tolower(static_cast<unsigned char>(a[0])) < tolower(static_cast<unsigned char>(b[0])));
 }
 
-void ls_go(char *argv, bool &a, bool &l, bool &R, string argname){
+void ls_go(char *argv, bool &a, bool &l, bool &R){
 	
 	struct stat s;
 	if(-1 == stat(argv, &s)) {
@@ -35,7 +35,8 @@ void ls_go(char *argv, bool &a, bool &l, bool &R, string argname){
 		}
 	
 		struct dirent *filespecs;
-		vector<char*> list, dirlist;
+		vector<char*> list;
+		vector<string> dirlist;
 
 		errno = 0;
 		while(NULL != (filespecs = readdir(dirp))){
@@ -52,32 +53,38 @@ void ls_go(char *argv, bool &a, bool &l, bool &R, string argname){
 				exit(1);
 			}
 
+			string arg2;
 			if(R){												//for -R
-				string arg2;
-				arg2.append(argname);
+				arg2.append(argv);
+//cout << arg2 << endl;
 				arg2.append("/");
+//cout << arg2 << endl;
 				arg2.append(filespecs->d_name);
-//
+//cout << arg2 << endl;
 				char *argg = new char[arg2.length()+1];
 				strcpy(argg, arg2.c_str());
-				arg2 = filespecs->d_name;
+		
 //	cout << '1' << arg2 << '2' << argg << endl;
 				if(-1 == stat(argg, &s)) {
-//cout << filespecs->d_name << " ";
-					perror("Error with stat");
+//cout << argg << " ";
+					perror("Error with stat2");
 					exit(1);
 				}
 //cout << filespecs->d_name << endl;
 				if((S_IFDIR & s.st_mode) )  {//is a directory
+//			cout << "0000" <<  argg << endl;
 					if ((a) && (strcmp(filespecs->d_name, ".") != 0) && (strcmp(filespecs->d_name, "..") != 0)) {
-							dirlist.push_back(filespecs->d_name);
+//			cout << "2222" << filespecs->d_name << endl;
+						dirlist.push_back(arg2);
 					}
 					else if((!a) && (filespecs->d_name[0] != '.')){
-							dirlist.push_back(filespecs->d_name);
+//			cout << "3333" << filespecs->d_name << endl;
+						dirlist.push_back(arg2);
 					}
 				}
 				delete[] argg;
 			}
+			
 		}
 		sort(list.begin(), list.end(), sort_alpha);
 		if(!list.empty()){
@@ -92,15 +99,20 @@ void ls_go(char *argv, bool &a, bool &l, bool &R, string argname){
 			exit(1);
 		}
 
-		for(unsigned int i = 0; i < dirlist.size(); i++){	
-//cout <<"R OKOK: " << i << '/' << dirlist.size() << " " << dirlist[i]<< endl;
-			string name(dirlist[i]);
-			name.insert(0, "/");
-			name.insert(0, argname);
-			strcpy(dirlist[i], name.c_str());
-			
-			cout << endl << dirlist[i] << ':' << endl; 
-			ls_go(dirlist[i], a, l, R, name);
+		if(R){
+			for(unsigned int i = 0; i < dirlist.size(); i++){		
+//		cout << "1111" << dirlist[i]<< dirlist.size() << endl;
+		//		string name(dirlist[i]);
+		//		name.insert(0, "/");
+		//		name.insert(0, argname);
+		//		strcpy(dirlist.at(i), name.c_str());
+	
+				cout << endl << dirlist[i] << ':' << endl;
+//				cout << name << ':' << endl;
+				char* name = new char[dirlist.at(i).size() +1];
+				strcpy(name, dirlist.at(i).c_str());
+				ls_go(name, a, l, R);
+			}
 		}
 
 	}
@@ -116,12 +128,12 @@ int main(int argc, char** argv){
 	bool a = false;
 	bool l = false;
 	bool R = false;
-	char here[2] = {"."};
+	char here[2]= {"."};
 	
 	if(argc == 1){								//regular ls
 	//	cout << "here" << endl;
-		string name(here);
-		ls_go(here, a, l, R, name);		
+	//	string name(here);
+		ls_go(here, a, l, R);		
 	}
 	else{
 		vector<char*> list;
@@ -145,21 +157,15 @@ int main(int argc, char** argv){
 		if(flags.find('R') != string::npos) R = true;
 
 		if(list.empty()) { 
-			string name(here);
-			ls_go(here, a, l, R, name);
+	//		string name(here);
+			ls_go(here, a, l, R);
 		}
 		else{
 			for(unsigned int i = 0; i < list.size(); i++){
-				string name(list[i]);
-				ls_go(list[i], a, l, R, name);
+	//			string name(list[i]);
+				ls_go(list[i], a, l, R);
 			}
 		}
-		
-	
-		
-
-
-
 	}
 
 
