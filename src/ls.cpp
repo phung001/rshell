c1efa1e172c8bef64b45c00fd96020209d40c6bd
Peter <Peter@Peters-MacBook-Air.local>
2015-04-29 20:48:54 -0700

    added -R, some bugs

--- src/ls.cpp
+++ src/ls.cpp
@@ -14,67 +14,101 @@
 
 using namespace std;
 
-string trim_space(string const& str){
-		if(str.empty()) return str;
-
-			size_t first = str.find_first_not_of(' ');
-				size_t last = str.find_last_not_of(' ');
-					return str.substr(first, last-first+1);
-}
-
-string trim_tab(string const& str){
-		if(str.empty()) return str;
-
-			size_t first = str.find_first_not_of('\t');
-				size_t last = str.find_last_not_of('\t');
-					return str.substr(first, last-first+1);
-}
-
-string trim(string const& str){
-		string temp = trim_space(str);
-			temp = trim_tab(temp);
-				return temp;
-}
-
 bool sort_alpha(char* a, char* b){
 	return (tolower(static_cast<unsigned char>(a[0])) < tolower(static_cast<unsigned char>(b[0])));
 }
 
-void ls_go(char *argv, bool a, bool l, bool R){
-	DIR *dirp;
-
-	if(NULL == (dirp = opendir(argv))){
-		perror("Error with opendir");
+void ls_go(char *argv, bool &a, bool &l, bool &R, string argname){
+	
+	struct stat s;
+	if(-1 == stat(argv, &s)) {
+		cout << "stat1" << argv  << endl;
+		perror("Error with stat");
 		exit(1);
 	}
+	if(S_IFDIR & s.st_mode) {							//is a directory
+		
+		DIR *dirp;
+		if(NULL == (dirp = opendir(argv))){
+			perror("Error with opendir");
+			exit(1);
+		}
 	
-	struct dirent *filespecs;
-	vector<char*> list;	
-	errno = 0;
-	while(NULL != (filespecs = readdir(dirp))){
-		if(!a){
-			if(filespecs->d_name[0] != '.'){
+		struct dirent *filespecs;
+		vector<char*> list, dirlist;
+
+		errno = 0;
+		while(NULL != (filespecs = readdir(dirp))){
+			if(!a){												//for regular ls
+				if(filespecs->d_name[0] != '.'){
+					list.push_back(filespecs->d_name);
+				}
+			}
+			else												//for -a
 				list.push_back(filespecs->d_name);
+	
+			if(errno != 0){
+				perror("Error with readdir");
+				exit(1);
+			}
+
+			if(R){												//for -R
+				string arg2;
+				arg2.append(argname);
+				arg2.append("/");
+				arg2.append(filespecs->d_name);
+//
+				char *argg = new char[arg2.length()+1];
+				strcpy(argg, arg2.c_str());
+				arg2 = filespecs->d_name;
+//	cout << '1' << arg2 << '2' << argg << endl;
+				if(-1 == stat(argg, &s)) {
+//cout << filespecs->d_name << " ";
+					perror("Error with stat");
+					exit(1);
+				}
+//cout << filespecs->d_name << endl;
+				if((S_IFDIR & s.st_mode) )  {//is a directory
+					if ((a) && (strcmp(filespecs->d_name, ".") != 0) && (strcmp(filespecs->d_name, "..") != 0)) {
+							dirlist.push_back(filespecs->d_name);
+					}
+					else if((!a) && (filespecs->d_name[0] != '.')){
+							dirlist.push_back(filespecs->d_name);
+					}
+				}
+				delete[] argg;
 			}
 		}
-		else
-			list.push_back(filespecs->d_name);
-	}
-	sort(list.begin(), list.end(), sort_alpha);
-	for(unsigned int i = 0; i < list.size(); i++){
-		cout <<list.at(i) << "  ";
-	}
-	cout << endl;
+		sort(list.begin(), list.end(), sort_alpha);
+		if(!list.empty()){
+			for(unsigned int i = 0; i < list.size(); i++){
+				cout <<list.at(i) << "  ";
+			}
+			cout << endl;
+		}
 
-	if(errno != 0){
-		perror("Error with readdir");
-		exit(1);
+		if(closedir(dirp) == -1){
+			perror("Error with closedir");
+			exit(1);
+		}
+
+		for(unsigned int i = 0; i < dirlist.size(); i++){	
+//cout <<"R OKOK: " << i << '/' << dirlist.size() << " " << dirlist[i]<< endl;
+			string name(dirlist[i]);
+			name.insert(0, "/");
+			name.insert(0, argname);
+			strcpy(dirlist[i], name.c_str());
+			
+			cout << endl << dirlist[i] << ':' << endl; 
+			ls_go(dirlist[i], a, l, R, name);
+		}
+
 	}
 
-	if(closedir(dirp) == -1){
-		perror("Error with closedir");
-		exit(1);
+	else {
+		cout << argv << endl;
 	}
+
 }
 
 int main(int argc, char** argv){
@@ -86,7 +120,8 @@ int main(int argc, char** argv){
 	
 	if(argc == 1){								//regular ls
 	//	cout << "here" << endl;
-		ls_go(here, a, l, R);		
+		string name(here);
+		ls_go(here, a, l, R, name);		
 	}
 	else{
 		vector<char*> list;
@@ -103,28 +138,28 @@ int main(int argc, char** argv){
 			}
 	
 		}
-		cout << flags << endl;
+	//	cout << flags << endl;
 		
 		if(flags.find('a') != string::npos) a = true;
 		if(flags.find('l') != string::npos) l = true;
 		if(flags.find('R') != string::npos) R = true;
 
-		if(list.empty()) ls_go(here, a, l, R);
+		if(list.empty()) { 
+			string name(here);
+			ls_go(here, a, l, R, name);
+		}
 		else{
 			for(unsigned int i = 0; i < list.size(); i++){
-				ls_go(list[i], a, l, R);
+				string name(list[i]);
+				ls_go(list[i], a, l, R, name);
 			}
 		}
 		
 	
-//		else if(strcmp(argv[1], "-R") == 0) ls_go(here, false, false, true);	//ls -R
 		
-//		else{
-//			cout << "Error: invalid parameter(s) entered." << endl;
 
 
 
-//		}
 	}
 
 
